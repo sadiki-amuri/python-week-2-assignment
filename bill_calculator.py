318a571560c00b73f2bc9bd6e78f78d50eb8037e
master tens kg sadikey <mastertenskgsadikey@gmail.com>
2026-09-19 02:11:56 -0700

    Fix formatting in print statement for total calculation

--- bill_calculator.py
+++ bill_calculator.py
@@ -4,4 +4,4 @@ price = float(input("enter the price of one item: "))
 quantity = int(input("enter the quantity: "))
 
 total = price * quantity
-print(f"{quantity} items at {price: .2f} each ={total: .2f}")
+print(f"{quantity} items at {price: .2f} each ={total:.2f}")
